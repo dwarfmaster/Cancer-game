20901511c2d9367b15ef68bc3475fc87bcff151a
lucas8 <luc.linux@mailoo.org>
2012-09-04 11:18:49 +0200

    Suppression de SDLP_sprite dans les dépendances.

--- src/compil.mk
+++ src/compil.mk
@@ -1,7 +1,7 @@
 CXXFLAGS=-Wall -Wextra
-CXXFLAGS+= `pkg-config --cflags SDLP_sprite SDLP_event SDLP_tools SDLP_position`
+CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position`
 CXXFLAGS+= `sdl-config --cflags`
-LDFLAGS=`pkg-config --libs SDLP_sprite SDLP_event SDLP_tools SDLP_position`
+LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position`
 LDFLAGS+= `sdl-config --libs`
 CC=g++
 
