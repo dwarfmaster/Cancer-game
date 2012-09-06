479e00e239ec3ce52008b323d1f00545af8ab21a
lucas8 <luc.linux@mailoo.org>
2012-09-06 18:57:02 +0200

    Ajout des directives de linkage manquantes pour la compilation.

--- src/compil.mk
+++ src/compil.mk
@@ -1,8 +1,9 @@
 CXXFLAGS=-Wall -Wextra
-CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position`
+CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image`
 CXXFLAGS+= `sdl-config --cflags`
-LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position`
+LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image`
 LDFLAGS+= `sdl-config --libs`
+LDFLAGS+= -ltinyxml -lboost_filesystem -lboost_system
 CC=g++
 
 
