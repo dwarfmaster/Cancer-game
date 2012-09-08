8be78c2161d19043cdfa601530d9253df23d74f0
lucas8 <luc.linux@mailoo.org>
2012-09-08 22:34:17 +0200

    Ajout de SDL_mixer aux dépendance de compilation et linkage.

--- src/compil.mk
+++ src/compil.mk
@@ -1,7 +1,7 @@
 CXXFLAGS=-Wall -Wextra
-CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image`
+CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer`
 CXXFLAGS+= `sdl-config --cflags`
-LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image`
+LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer`
 LDFLAGS+= `sdl-config --libs`
 LDFLAGS+= -ltinyxml -lboost_filesystem -lboost_system -lboost_program_options
 CC=g++
