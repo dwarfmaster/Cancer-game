9948c2a72604ac32fee7e839dedf4c819832d170
lucas8 <luc.linux@mailoo.org>
2012-09-12 13:42:43 +0200

    Ajout de SDL_ttf aux dépendances.

--- src/compil.mk
+++ src/compil.mk
@@ -1,7 +1,7 @@
 CXXFLAGS=-Wall -Wextra
-CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer`
+CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer SDL_ttf`
 CXXFLAGS+= `sdl-config --cflags`
-LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer`
+LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer SDL_ttf`
 LDFLAGS+= `sdl-config --libs`
 LDFLAGS+= -ltinyxml -lboost_filesystem -lboost_system -lboost_program_options
 CC=g++
