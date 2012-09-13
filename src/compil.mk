18ff71a538c1f959d41274c0d87056b249519178
lucas8 <luc.linux@mailoo.org>
2012-09-13 19:11:33 +0200

    Suppression de SDL_ttf des dépendances.

--- src/compil.mk
+++ src/compil.mk
@@ -1,7 +1,7 @@
 CXXFLAGS=-Wall -Wextra
-CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer SDL_ttf guichan-0.8 guichan_sdl-0.8`
+CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer guichan-0.8 guichan_sdl-0.8`
 CXXFLAGS+= `sdl-config --cflags`
-LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer SDL_ttf guichan-0.8 guichan_sdl-0.8`
+LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer guichan-0.8 guichan_sdl-0.8`
 LDFLAGS+= `sdl-config --libs`
 LDFLAGS+= -ltinyxml -lboost_filesystem -lboost_system -lboost_program_options
 CC=g++
