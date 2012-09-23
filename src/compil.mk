def143d66be4a3449614bc383dce494b7fdb97be
lucas8 <luc.linux@mailoo.org>
2012-09-23 11:28:32 +0200

    Passage de la compilation en mode debug.

--- src/compil.mk
+++ src/compil.mk
@@ -4,6 +4,7 @@ RCDIR=/usr/share/$(INSTALLNAME)/
 CXXFLAGS=-Wall -Wextra
 CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer SDL_gfx guichan-0.8 guichan_sdl-0.8`
 CXXFLAGS+= `sdl-config --cflags`
+CXXFLAGS+= -g
 CXXFLAGS+= -DRCDIR="\"$(RCDIR)\"" -DFNAME="\"$(INSTALLNAME)\""
 LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer SDL_gfx guichan-0.8 guichan_sdl-0.8`
 LDFLAGS+= `sdl-config --libs`
