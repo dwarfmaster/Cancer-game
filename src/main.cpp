c24f31a82d219669174d09d5be79580f3388161f
lucas8 <luc.linux@mailoo.org>
2012-09-23 10:02:36 +0200

    Ajout de la gestion des exception dans main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
+#include "core/exception.hpp"
+#include "i18n.hpp"
+#include <guichan.hpp>
+#include <exception>
 
 int main(int argc, char *argv[])
 {
+	try{
+	}
+	catch(const core::Exception& e)
+	{
+		std::cerr << _i("A game error appened : ") << e.what() << std::endl;
+		return 1;
+	}
+	catch(const gcn::Exception& e)
+	{
+		std::cerr << _i("A guichan error appened : \"") << e.getMessage() 
+			// Est suivit d'un nom de fonction
+			<< _i("\" in ") << e.getFunction() << std::endl;
+		return 1;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << _i("An internal error appened : ") << e.what() << std::endl;
+		return 1;
+	}
+	catch(...)
+	{
+		std::cerr << _i("An unknown error appened.") << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
 
