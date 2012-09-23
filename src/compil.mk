c292d54f47e29f2754d6ded434046c6fbe8fe521
lucas8 <luc.linux@mailoo.org>
2012-09-23 11:14:08 +0200

    Ajout de SDL_gfx aux dépendances.

--- src/compil.mk
+++ src/compil.mk
@@ -2,10 +2,10 @@
 INSTALLNAME=cancer-game
 RCDIR=/usr/share/$(INSTALLNAME)/
 CXXFLAGS=-Wall -Wextra
-CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer guichan-0.8 guichan_sdl-0.8`
+CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer SDL_gfx guichan-0.8 guichan_sdl-0.8`
 CXXFLAGS+= `sdl-config --cflags`
 CXXFLAGS+= -DRCDIR="\"$(RCDIR)\"" -DFNAME="\"$(INSTALLNAME)\""
-LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer guichan-0.8 guichan_sdl-0.8`
+LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer SDL_gfx guichan-0.8 guichan_sdl-0.8`
 LDFLAGS+= `sdl-config --libs`
 LDFLAGS+= -ltinyxml -lboost_filesystem -lboost_system -lboost_program_options -lboost_regex
 CC=g++
