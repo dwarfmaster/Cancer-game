f2cbd3e4b37827886255cf17fe9f3bd1b7502614
lucas8 <luc.linux@mailoo.org>
2012-11-08 16:49:34 +0100

    Début de l'interface de MutedCell.

---
+++ src/mutedcell.cpp
@@ -0,0 +1,6 @@
+
+#include "mutedcell.hpp"
+
+size_t MutedCell::m_nb = 0;
+SDL_Surface* MutedCell::m_img = NULL;
+
---
+++ src/mutedcell.hpp
@@ -0,0 +1,55 @@
+
+#ifndef DEF_MUTEDCELL
+#define DEF_MUTEDCELL
+
+#include <list>
+#include <SDL/SDL.h>
+#include "cell.hpp"
+
+class SaneCell; // TODO créer la sanecell
+
+class MutedCell;
+void update(MutedCell* cell);
+
+class MutedCell : public Cell
+{
+	friend void update(MutedCell* cell);
+
+	public:
+		MutedCell();
+		virtual ~MutedCell();
+
+		virtual SDL_Surface* getImg() const;
+		virtual std::string save() const;
+
+		static size_t nbMuted();
+		static MutedCell* load(const std::string& src);
+
+		size_t attackers() const; // Nombre d'attackers
+		void createAttacker(SaneCell* dest);
+
+	private:
+		Uint32 m_lastTime;
+
+		// Les attackers
+		typedef std::list<SaneCell*> listCell;
+		typedef listCell::iterator cells_it;
+		typedef listCell::const_iterator cells_cit;
+		listCell m_attackers; // Les attackers à créer
+		Uint32 m_timeSpend; // Temps passé depuis le début de la création
+
+		// TODO les médiateurs
+
+		void selfUpdate();
+
+		static SDL_Surface* m_img;
+
+		static size_t m_nb;
+		typedef std::list<MutedCell*> all_t;
+		typedef all_t::iterator all_it;
+		typedef all_t::const_iterator all_cit;
+		all_t m_all;
+};//MutedCell
+
+#endif//DEF_MUTEDCELL
+
