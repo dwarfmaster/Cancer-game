c8f6b81c4762221c511adf8fe2e32235fd860628
lucas8 <luc.linux@mailoo.org>
2012-11-08 10:47:33 +0100

    Début de l'implémentation de Cell::load.

--- src/cell.cpp
+++ src/cell.cpp
@@ -1,6 +1,10 @@
 
 #include "cell.hpp"
 
+#include <sstream>
+#include "core/exception.hpp"
+#include "i18n.hpp"
+
 size_t Cell::m_cellCount = 0;
 
 Cell::Cell()
@@ -20,7 +24,33 @@ size_t Cell::nbCells()
 
 Cell* Cell::load(const std::string& src)
 {
-	// TODO
+	size_t pos = src.find_first_of(':');
+
+	if(pos == std::string::npos)
+	{
+		std::ostringstream oss;
+		oss << _i("Error while parsing a cell description \"") << src << _i("\" : there is no ':' separation");
+		throw core::Exception( oss.str() );
+	}
+
+	std::string type = src.substr(0, pos - 1);
+	std::string arg = src.substr(pos);
+	if(type == "muted")
+	{
+		// TODO : return MutedCell::load(arg);
+	}
+	else if(type == "sane")
+	{
+		// TODO : return SaneCell::load(arg);
+	}
+	else
+	{
+		std::ostringstream oss;
+		oss << _i("Error while parsing a cell description : type\"") << type << _i("\" is invalid");
+		throw core::Exception( oss.str() );
+	}
+
+	return NULL; // Ne doit pas arriver, pour éviter les warnings
 }
 
 
