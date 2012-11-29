a0898d44d5a35010fe2e2a58ffb7f783b416a9f5
lucas8 <luc.linux@mailoo.org>
2012-11-29 17:23:50 +0100

    Ajout de la fonction core::CutByChar : sépare une chaine selon un char.

--- src/core/strtools.cpp
+++ src/core/strtools.cpp
@@ -1,7 +1,20 @@
 
 #include "strtools.hpp"
 
+#include <sstream>
+
 namespace core
 {
+	std::vector<std::string> cutByChar(const std::string& src, char del)
+	{
+		std::istringstream iss(src);
+		std::vector<std::string> parts;
+
+		std::string part;
+		while( std::getline(iss, part, del) )
+			parts.push_back(part);
+
+		return parts;
+	}
 };//namespace core
 
--- src/core/strtools.hpp
+++ src/core/strtools.hpp
@@ -2,8 +2,12 @@
 #ifndef DEF_CORE_STRTOOLS
 #define DEF_CORE_STRTOOLS
 
+#include <vector>
+#include <string>
+
 namespace core
 {
+	std::vector<std::string> cutByChar(const std::string& src, char del);
 };//namespace core
 
 #endif//DEF_CORE_STRTOOLS
