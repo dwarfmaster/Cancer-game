cd1c096ebd157278265b6b6c45afa88cf0bffd73
lucas8 <luc.linux@mailoo.org>
2012-12-03 20:10:11 +0100

    Adaptation de la compilation à la nouvelle norme c++.

--- src/compil.mk
+++ src/compil.mk
@@ -6,6 +6,7 @@ CXXFLAGS+= `pkg-config --cflags SDLP_event SDLP_tools SDLP_position SDL_image SD
 CXXFLAGS+= `sdl-config --cflags`
 CXXFLAGS+= -g
 CXXFLAGS+= -DRCDIR="\"$(RCDIR)\"" -DFNAME="\"$(INSTALLNAME)\""
+CXXFLAGS+= -std=gnu++0x
 LDFLAGS=`pkg-config --libs SDLP_event SDLP_tools SDLP_position SDL_image SDL_mixer SDL_gfx guichan-0.8 guichan_sdl-0.8`
 LDFLAGS+= `sdl-config --libs`
 LDFLAGS+= -ltinyxml -lboost_filesystem -lboost_system -lboost_program_options -lboost_regex
