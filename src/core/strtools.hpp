3b473a5878a3e516572f1b9f7968d4c7fa2bab9f
lucas8 <luc.linux@mailoo.org>
2012-12-15 19:39:46 +0100

    Ajout des fonctions toString et fromString dans strtools.

--- src/core/strtools.hpp
+++ src/core/strtools.hpp
@@ -4,10 +4,25 @@
 
 #include <vector>
 #include <string>
+#include <sstream>
 
 namespace core
 {
 	std::vector<std::string> cutByChar(const std::string& src, char del);
+
+	template<typename T>
+		std::string toString(const T& value)
+		{
+			std::istringstream oss(value);
+			return oss.str();
+		}
+
+	template<typename T>
+		bool fromString(const std::string& str, T& dest)
+		{
+			std::istringstream iss(str);
+			return iss >> dest != 0;
+		}
 };//namespace core
 
 #endif//DEF_CORE_STRTOOLS
